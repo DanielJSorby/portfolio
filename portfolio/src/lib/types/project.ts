170ad0d9d62d165620bc9c7da30b673033dfacb6
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-01-02 12:24:49 +0100

    refactor: move projects data to JSON file

---
+++ portfolio/src/lib/types/project.ts
@@ -0,0 +1,13 @@
+export interface Project {
+	title: {
+		no: string;
+		en: string;
+	};
+	description: {
+		no: string;
+		en: string;
+	};
+	technologies: string[];
+	image: string;
+	link: string;
+}
--- portfolio/src/routes/projects/+page.svelte
+++ portfolio/src/routes/projects/+page.svelte
@@ -1,109 +1,20 @@
-<script>
+<script lang="ts">
 	import { language, translations } from '$lib/stores/language';
 	import { techColors } from '$lib/utils/techColors';
+	import projectsData from '$lib/data/projects.json';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import type { Project } from '$lib/types/project';
+	import type { TechColors } from '$lib/types/techColors';
 	
 	$: t = translations[$language];
 
-	const projects = [
-		{
-			title: {
-				no: 'Dasi GPT',
-				en: 'Dasi GPT'
-			},
-			description: {
-				no: 'En ai chatbot som hjelper deg med alt du trenger!',
-				en: 'An ai chatbot that helps you with everything you need!'
-			},
-			technologies: ['Svelte Kit', 'TypeScript', 'OpenAI', 'MySQL', 'Linux', 'Git', 'Markdown'],
-			image: 'https://picsum.photos/800/400?random=1',
-			link: 'https://github.com/DanielJSorby/DASI'
-		},
-		{
-			title: {
-				no: 'Prime Wheels',
-				en: 'Prime Wheels'
-			},
-			description: {
-				no: 'Et bilnettsted for å kjøpe biler. Laget som en skoleprosjekt.',
-				en: 'A car website for buying cars. Made as a school project.'
-			},
-			technologies: ['HTML', 'CSS', 'JavaScript'],
-			image: 'https://picsum.photos/800/400?random=2',
-			link: 'https://github.com/DanielJSorby/DanielOgSimen'
-		},
-		{
-			title: {
-				no: 'IMDB',
-				en: 'IMDB'
-			},
-			description: {
-				no: 'En nettside hvor du kan se informasjon om filmer.',
-				en: 'A website where you can see information about movies.'
-			},
-			technologies: ['HTML', 'CSS', 'JavaScript'],
-			image: 'https://picsum.photos/800/400?random=3',
-			link: 'https://github.com/DanielJSorby/imdb'
-		},
-		{
-			title: {
-				no: 'Flow',
-				en: 'Flow'
-			},
-			description: {
-				no: 'Nettsiden til energidrikken flow. Laget som et bedriftprosjekt på skolen',
-				en: 'The website for the energy drink flow. Created as a business project at school'
-			},
-			technologies: ['Svelte Kit', 'Json', 'JavaScript'],
-			image: 'https://picsum.photos/800/400?random=4',
-			link: 'https://github.com/DanielJSorby/flow-svelte'
-		},
-		{
-			title: {
-				no: 'Flask Logg Inn',
-				en: 'Flask Log In'
-			},
-			description: {
-				no: 'Et prosjekt for å lære Flask, med et enkelt innloggingsskjema som sjekker mot en bruker-JSON-fil.',
-				en: 'A project to learn Flask, featuring a simple login form that validates against a user JSON file.'
-			},
-			technologies: ['Python', 'Json', 'JavaScript', 'HTML'],
-			image: 'https://picsum.photos/800/400?random=5',
-			link: 'https://github.com/DanielJSorby/flask'
-		},
-        {
-			title: {
-				no: 'Lære React',
-				en: 'Learn React'
-			},
-			description: {
-				no: 'Prosjektet jeg brukte når jeg lærte meg React',
-				en: 'The project I used to learn React'
-			},
-			technologies: ['React', 'TypeScript', 'CSS', 'HTML'],
-			image: 'https://picsum.photos/800/400?random=6',
-			link: 'https://github.com/DanielJSorby/Learn-React'
-		},
-        {
-			title: {
-				no: 'Github Readme',
-				en: 'Github Readme'
-			},
-			description: {
-				no: 'Readme filen til Github Profilen min',
-				en: 'The Readme file to my Github Profile'
-			},
-			technologies: ['Markdown'],
-			image: 'https://picsum.photos/800/400?random=7',
-			link: 'https://github.com/DanielJSorby/DanielJSorby'
-		}
-	];
+	const projects: Project[] = projectsData.projects;
 
 	// Get unique technologies from all projects
 	const allTechnologies = [...new Set(projects.flatMap(p => p.technologies))].sort();
 	
-	let selectedTech = '';
+	let selectedTech: string = '';
 
 	onMount(() => {
 		const techParam = $page.url.searchParams.get('tech');
@@ -113,11 +24,15 @@
 	});
 
 	$: filteredProjects = selectedTech 
-		? projects.filter(project => project.technologies.includes(selectedTech))
+		? projects.filter((project: Project) => project.technologies.includes(selectedTech))
 		: projects;
 
-	function selectTechnology(tech) {
+	function selectTechnology(tech: string): void {
 		selectedTech = selectedTech === tech ? '' : tech;
+	}
+
+	function getTechColor(tech: string): string {
+		return (techColors as TechColors)[tech] || '#666666';
 	}
 </script>
 
@@ -132,7 +47,7 @@
 			<button 
 				class="tech-button" 
 				class:active={selectedTech === tech}
-				style="--tech-color: {techColors[tech] || '#666666'}"
+				style="--tech-color: {getTechColor(tech)}"
 				on:click={() => selectTechnology(tech)}
 			>
 				{tech}
@@ -155,7 +70,7 @@
 						<span 
 							class="tech-tag" 
 							class:highlight={selectedTech === tech}
-							style="--tech-color: {techColors[tech] || '#666666'}"
+							style="--tech-color: {getTechColor(tech)}"
 							on:click={() => selectTechnology(tech)}
 						>
 							{tech}
